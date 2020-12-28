b863e7bd4c7258e1523c6aa3f145b31e1740926e
Dipen Parihar <parihar.dipen@gmail.com>
2020-12-28 17:52:24 +0530

    Change in snake eyes and gameboard background changes

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import food from '../assets/food1.png';
+// import Apple from '../assets/apple.svg';
+import { ReactComponent as Apple } from '../assets/apple.svg';
+
 const SnakeFood = styled.div`
   position: absolute;
   width: 22px;
   height: 22px;
-  background-image: url(${food});
+  // background-image: url(${food});
   background-size: cover;
   background-repeat: no-repeat;
   z-index: 3;
@@ -14,7 +17,11 @@ const SnakeFood = styled.div`
 `;
 const Food = ({ dot }) => {
   if (dot) {
-    return <SnakeFood left={dot[0]} top={dot[1]}></SnakeFood>;
+    return (
+      <SnakeFood left={dot[0]} top={dot[1]}>
+        <Apple width="22px" height="22px" />
+      </SnakeFood>
+    );
   }
 };
 export default React.memo(Food);
--- client/src/containers/GameBoard/GameBoardStyle.js
+++ client/src/containers/GameBoard/GameBoardStyle.js
@@ -57,6 +57,7 @@ export const GameArea = styled.div`
   border-image: url(${stone}) 20 round;
   border-image-repeat: repeat;
   background: url(${grass}) left top no-repeat;
+  box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.2);
   background-size: cover;
   padding: 30px;
 `;
